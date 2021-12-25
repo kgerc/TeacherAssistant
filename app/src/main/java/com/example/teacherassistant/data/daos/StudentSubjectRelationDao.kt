36b4f3efa27462022a773df0891707feafe5798b
Krzysztof Gerc <krzysztofgerc10@gmail.com>
2021-12-25 11:24:45 +0100

    Added Subject and StudentSubjectRelation data access objects

---
+++ app/src/main/java/com/example/teacherassistant/data/daos/StudentSubjectRelationDao.kt
@@ -0,0 +1,35 @@
+package com.example.teacherassistant.data.daos
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.teacherassistant.data.models.StudentSubjectRelation
+
+@Dao
+interface StudentSubjectDao {
+    val all: LiveData<List<StudentSubjectRelation>>
+        @Query("SELECT * FROM student_to_subject_rel") get
+
+    @Query("SELECT * FROM student_to_subject_rel WHERE id IN (:IDs)")
+    fun getRangeByID(IDs: Long): List<StudentSubjectRelation>
+
+    @Query("SELECT * FROM student_to_subject_rel WHERE id = :ID")
+    fun getById(ID: Long): StudentSubjectRelation
+
+    @Query("SELECT * FROM student_to_subject_rel where student_id = :studentId")
+    fun getByStudent(studentId: Long): List<StudentSubjectRelation>
+
+    @Query("SELECT * FROM student_to_subject_rel where subject_id = :subjectId")
+    fun getBySubject(subjectId: Long): List<StudentSubjectRelation>
+
+    @Query("select * from student_to_subject_rel where student_id = :studentId and subject_id = :subjectId limit 1")
+    fun findForStudentAndSubject(studentId: Long, subjectId: Long): StudentSubjectRelation
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(vararg student_to_subject: StudentSubjectRelation)
+
+    @Delete
+    fun delete(student_to_subject: StudentSubjectRelation)
+
+    @Update
+    fun update(student_to_subject: StudentSubjectRelation)
+}
---
+++ app/src/main/java/com/example/teacherassistant/data/daos/SubjectDao.kt
@@ -0,0 +1,32 @@
+package com.example.teacherassistant.data.daos
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.teacherassistant.data.models.Subject
+
+@Dao
+interface SubjectDao {
+    val all: LiveData<List<Subject>>
+        @Query("SELECT * FROM subjects") get
+
+    @Query("SELECT * FROM subjects WHERE id IN (:IDs)")
+    fun getRangeByID(IDs: LongArray): List<Subject>
+
+    @Query("SELECT * FROM subjects WHERE id = :ID")
+    fun getById(ID: Long): Subject
+
+    @Query("SELECT * FROM subjects WHERE name LIKE :name LIMIT 1")
+    fun getByName(name: String): Subject
+
+    @Insert
+    fun insertRange(vararg subject: Subject)
+
+    @Insert
+    fun insert (subject: Subject): Long
+
+    @Update
+    fun update(subject: Subject)
+
+    @Delete
+    fun delete(subject: Subject)
+}
